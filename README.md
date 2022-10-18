c244e14ecdcdad5191fecfa0957305c7fa47e04f
scott2b <@scott2b>
2022-10-18 18:07:58 -0500

    adds templating

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fusionauth-example-python-starlette
 Example FusionAuth client application demonstrating user registration and authentication
+
+## Execution
+
+```
+uvicorn main:app --reload
+```
---
+++ main.py
@@ -0,0 +1,18 @@
+from starlette.applications import Starlette
+from starlette.routing import Route, Mount
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
+
+templates = Jinja2Templates(directory='templates')
+
+async def homepage(request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+routes = [
+    Route('/', endpoint=homepage),
+    Mount('/static', StaticFiles(directory='static'), name='static')
+]
+
+app = Starlette(debug=True, routes=routes)
+
